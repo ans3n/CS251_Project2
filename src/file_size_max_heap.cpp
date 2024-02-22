6cbac8893638fe2fb9ccb01623a078025c36aeea
Ansen Tsan <139832422+ans3n@users.noreply.github.com>
2024-02-22 11:46:07 -0500

    push function first late

--- src/file_size_max_heap.cpp
+++ src/file_size_max_heap.cpp
@@ -3,7 +3,26 @@ using namespace cs251;
 
 void file_size_max_heap::push(const size_t fileSize, const handle handle)
 {
+    file_size_max_heap_node newNode;
+    newNode.m_handle = handle;
+    newNode.m_value = fileSize;
+    m_nodes.push_back(newNode);
 
+    //heapify up for max heap
+    size_t currentIndex = m_nodeSize;
+    while (currentIndex > 0) {
+        size_t parentIndex = (currentIndex - 1) / 2;
+        if (m_nodes[currentIndex].m_value > m_nodes[parentIndex].m_value) {
+            // Swap nodes if the current node has a larger value than its parent
+            std::swap(m_nodes[currentIndex], m_nodes[parentIndex]);
+            currentIndex = parentIndex;
+        } else {
+            // already in position
+            break;
+        }
+    }
+
+    m_nodeSize++;
 }
 
 handle file_size_max_heap::top() const
@@ -14,6 +33,5 @@ handle file_size_max_heap::top() const
 
 void file_size_max_heap::remove(const handle handle)
 {
-	//TODO: Remove following line and add your implementation here.
-	throw std::logic_error("file_size_max_heap::" + std::string(__FUNCTION__) + " not implemented");
+
 }
